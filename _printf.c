b447a0cd61e2a13ca82ed04f724d61798f04c1bc
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-17 23:45:32 +0000

    p

--- _printf.c
+++ _printf.c
@@ -5,41 +5,4 @@
  * @...: infinit number of argument
  * Return: print
  */
-int _printf(const char *format, ...)
-{
-	va_list args;
 
-	va_start(args, format);
-
-	int count = 0;
-
-	while (*format != '\0')
-	{
-	if (*format != '%')
-	{
-	putchar(*format);
-	count++;
-	}
-	else
-	{
-	switch (*++format)
-	{
-	case 'c':
-	count += fprintf(stdout, "%c", va_arg(args, char));
-	break;
-	case 's':
-	count += fprintf(stdout, "%s", va_arg(args, char *));
-	break;
-	default:
-	putchar('%');
-	putchar(*format);
-	count += 2;
-	break;
-	}
-	}
-	format++;
-	}
-
-	va_end(args);
-	return (count);
-}
